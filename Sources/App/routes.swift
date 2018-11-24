bba6a5d6fd083fbf617abc7843b32eefbd2512a2
Cory Steers <developer@madsteer.com>
2018-11-23 23:17:20 -0600

    add custom route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,10 +12,37 @@ public func routes(_ router: Router) throws {
         return try req.view().render("home", context)
     }
 
-    router.get("staff") { req in
-        return "Meet our great team"
-    }
+    router.get("staff", String.parameter) { req -> Future<View> in
+        let name = try req.parameters.next(String.self)
 
+        // create some dummy data to work with
+        let bios = [
+            "kirk": "My name is James Kirk and I love snakes.",
+            "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+            "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+            "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+            "archer": "My name is Jonathan Archer and beagles are my thing."
+        ]
+
+        // define the struct we'll pass to the template
+        struct StaffView: Codable {
+            var name: String?
+            var bio: String?
+        }
+
+        let context: StaffView
+
+        // attempt to find a staff member by this name and fill in our struct
+        if let bio = bios[name] {
+            context = StaffView(name: name, bio: bio)
+        } else {
+            context = StaffView()
+        }
+
+        // render the template with whatever we have
+        return try req.view().render("staff", context)
+    }
+    
     router.get("contact") { req -> Future<View> in
         let context = [String: String]()
         return try req.view().render("contact", context)
