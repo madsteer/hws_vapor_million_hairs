c836e40dcc1dfd23a2f4206c11c9eaecd2ba2367
Cory Steers <developer@madsteer.com>
2018-11-23 23:07:55 -0600

    working with simple strings as html pages

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,22 @@
 import Routing
 import Vapor
+import Foundation
+import Leaf
 
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get { req in
+        return "Welcome to Million Hairs"
     }
+
+    router.get("staff") { req in
+        return "Meet our great team"
+    }
+
+    router.get("contact") { req in
+        return "Get in touch with us"
+    }
+
 }
