ee9f0817367487c43bf28cd9692afe3dd2b9d8ba
Cory Steers <developer@madsteer.com>
2018-11-23 23:09:09 -0600

    add some leaf routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,16 +7,18 @@ import Leaf
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get { req in
-        return "Welcome to Million Hairs"
+    router.get { req -> Future<View> in
+        let context = [String: String]()
+        return try req.view().render("home", context)
     }
 
     router.get("staff") { req in
         return "Meet our great team"
     }
 
-    router.get("contact") { req in
-        return "Get in touch with us"
+    router.get("contact") { req -> Future<View> in
+        let context = [String: String]()
+        return try req.view().render("home", context)
     }
 
 }
