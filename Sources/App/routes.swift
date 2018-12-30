fd70918469ecfc69760be2648d531ec720b11f47
Cory Steers <developer@madsteer.com>
2018-12-29 22:31:06 -0600

    fix staff page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,41 +12,44 @@ public func routes(_ router: Router) throws {
         return try req.view().render("home", context)
     }
 
-    router.get("staff", String.parameter) { req -> Future<View> in
-        let name = try req.parameters.next(String.self)
-
-        // create some dummy data to work with
-        let bios = [
-            "kirk": "My name is James Kirk and I love snakes.",
-            "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
-            "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
-            "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
-            "archer": "My name is Jonathan Archer and beagles are my thing."
-        ]
-
-        // define the struct we'll pass to the template
-        struct StaffView: Codable {
-            var name: String?
-            var bio: String?
-            var allNames: [String]
-        }
-
-        let context: StaffView
-
-        // attempt to find a staff member by this name and fill in our struct
-        if let bio = bios[name] {
-            context = StaffView(name: name, bio: bio, allNames: bios.keys.sorted())
-        } else {
-            context = StaffView(name: nil, bio: nil, allNames: bios.keys.sorted())
-        }
-        
-        // render the template with whatever we have
-        return try req.view().render("staff", context)
-    }
+    router.get("staff", use: handleStaffPage)
+    router.get("staff", String.parameter, use: handleStaffPage)
     
     router.get("contact") { req -> Future<View> in
         let context = [String: String]()
         return try req.view().render("contact", context)
     }
 
+}
+
+func handleStaffPage(_ req: Request) throws -> Future<View> {
+    let name = (try? req.parameters.next(String.self)) ?? "notProvided"
+
+    // create some dummy data to work with
+    let bios = [
+        "kirk": "My name is James Kirk and I love snakes.",
+        "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+        "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+        "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+        "archer": "My name is Jonathan Archer and beagles are my thing."
+    ]
+
+    // define the struct we'll pass to the template
+    struct StaffView: Codable {
+        var name: String?
+        var bio: String?
+        var allNames: [String]
+    }
+
+    let context: StaffView
+
+    // attempt to find a staff member by this name and fill in our struct
+    if let bio = bios[name] {
+        context = StaffView(name: name, bio: bio, allNames: bios.keys.sorted())
+    } else {
+        context = StaffView(name: nil, bio: nil, allNames: bios.keys.sorted())
+    }
+
+    // render the template with whatever we have
+    return try req.view().render("staff", context)
 }
